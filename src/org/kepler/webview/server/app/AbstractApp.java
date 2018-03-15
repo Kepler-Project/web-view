6d819d9fc0b665ce491dc35ef795d9fb606f878d
crawl <danielcrawl@gmail.com>
2018-03-15 11:03:35 -0700

    copyright

--- src/org/kepler/webview/server/app/AbstractApp.java
+++ src/org/kepler/webview/server/app/AbstractApp.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * '$Author: crawl $'
+ * '$Date: 2017-09-04 12:58:25 -0700 (Mon, 04 Sep 2017) $' 
+ * '$Revision: 1406 $'
+ * 
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the above
+ * copyright notice and the following two paragraphs appear in all copies
+ * of this software.
+ *
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ * FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ * THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ * PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ * CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
 package org.kepler.webview.server.app;
 
 import io.vertx.core.AsyncResult;
@@ -6,6 +33,10 @@ import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
+/** Base class for App.
+ * 
+ *  @author Daniel Crawl
+ */
 public abstract class AbstractApp implements App {
 
     @Override
