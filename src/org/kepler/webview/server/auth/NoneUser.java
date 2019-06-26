d97b5cc5864b0021a5c3c99fe6d207c12bf60419
crawl <danielcrawl@gmail.com>
2019-06-26 13:51:25 -0700

    adding ClusterSerializable methods

--- src/org/kepler/webview/server/auth/NoneUser.java
+++ src/org/kepler/webview/server/auth/NoneUser.java
@@ -1,12 +1,14 @@
 package org.kepler.webview.server.auth;
 
+import java.nio.charset.StandardCharsets;
+
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AbstractUser;
 import io.vertx.ext.auth.AuthProvider;
-import io.vertx.ext.auth.User;
 
 public class NoneUser extends AbstractUser {
 
@@ -36,6 +38,34 @@ public class NoneUser extends AbstractUser {
         permittedHandler.handle(Future.succeededFuture(Boolean.TRUE));
     }
 
+    @Override
+    public int readFromBuffer(int pos, Buffer buffer) {
+        pos = super.readFromBuffer(pos, buffer);
+        
+        int groupLength = buffer.getInt(pos);
+        pos += 4;
+        byte[] groupBytes = buffer.getBytes(pos, pos + groupLength);
+        pos += groupLength;
+        _group = new String(groupBytes, StandardCharsets.UTF_8);
+
+        int usernameLength = buffer.getInt(pos);
+        pos += 4;
+        byte[] usernameBytes = buffer.getBytes(pos, pos + usernameLength);
+        pos += usernameLength;
+        _username = new String(usernameBytes, StandardCharsets.UTF_8);
+
+        return pos;
+    }
+    
+    @Override
+    public void writeToBuffer(Buffer buffer) {
+        super.writeToBuffer(buffer);
+        byte[] groupBytes = _group.getBytes(StandardCharsets.UTF_8);
+        buffer.appendInt(groupBytes.length).appendBytes(groupBytes);
+        byte[] userBytes = _username.getBytes(StandardCharsets.UTF_8);
+        buffer.appendInt(userBytes.length).appendBytes(userBytes);        
+    }
+    
     private String _group;
     private String _username;
 }
