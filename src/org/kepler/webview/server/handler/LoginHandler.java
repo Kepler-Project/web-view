611253ad8c630b7fd7d0cc25d7f7a5c54074ab6c
crawl <danielcrawl@gmail.com>
2020-05-11 11:59:31 -0700

    removing duplicate code and changing how admin is determined

--- src/org/kepler/webview/server/handler/LoginHandler.java
+++ src/org/kepler/webview/server/handler/LoginHandler.java
@@ -29,9 +29,10 @@
 package org.kepler.webview.server.handler;
 
 import java.net.HttpURLConnection;
-import org.kepler.webview.server.WebViewConfiguration;
+
+import org.kepler.webview.server.MetadataUtilities;
 import org.kepler.webview.server.WebViewServer;
-import io.vertx.core.file.FileSystem;
+
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
@@ -45,8 +46,6 @@ public class LoginHandler extends BaseHandler {
 
     public LoginHandler(WebViewServer server) {
         super(server);
-        _metadataFileName = WebViewConfiguration.getHttpServerMetadataFileName();
-        _fileSystem = _server.getVertx().fileSystem();
     }
 
     @Override
@@ -60,165 +59,85 @@ public class LoginHandler extends BaseHandler {
             context.session().id());
          */
         
-        // get the last modified time of the metadata file.
-        _fileSystem.props(_metadataFileName, propsResult -> {
-            if(propsResult.succeeded()) {
-                // see if file was modified since we last read it
-                if(propsResult.result().lastModifiedTime() > _metadataLastModifiedTime || _metadataJson == null) {
-                    _metadataLastModifiedTime = propsResult.result().lastModifiedTime();
-                    
-                    // read metadata file
-                    _fileSystem.readFile(_metadataFileName, readResult -> {
-                        if(readResult.succeeded()) {
-                            // update the cached json
-                            try {
-                                
-                                JsonArray newMetadata = readResult.result().toJsonArray();
-                                for(int i = 0; i < newMetadata.size(); i++) {
-                                    if(newMetadata.getJsonObject(i).containsKey("enable") &&
-                                        !newMetadata.getJsonObject(i).getBoolean("enable").booleanValue()) {
-
-                                        newMetadata.remove(i);
-                                        
-                                        i--;
-                                    }
-                                }
-                                _metadataJson = newMetadata;
-                            } catch(Throwable t) {
-                                context.response()
-                                    .putHeader("Content-Type", "text/plain")
-                                    .setStatusCode(HttpURLConnection.HTTP_INTERNAL_ERROR)
-                                    .setStatusMessage("Could not access metadata.")
-                                    .end();
-                                System.err.println("Error reading metadata file " +
-                                    _metadataFileName + ": " + t.getMessage());
-                                _server.log(context.request(), context.user(), HttpURLConnection.HTTP_INTERNAL_ERROR, timestamp);
-                                return;
-                            }
-                            // send metadata json in response
-                            _sendMetadata(context, timestamp);
-                        } else {
-                            context.response()
-                                .putHeader("Content-Type", "text/plain")
-                                .setStatusCode(HttpURLConnection.HTTP_INTERNAL_ERROR)
-                                .setStatusMessage("Could not access metadata.")
-                                .end();
-                            System.err.println("Could not read metadata " +
-                                    _metadataFileName + ": " + readResult.cause().getMessage());
-                            _server.log(context.request(), context.user(), HttpURLConnection.HTTP_INTERNAL_ERROR, timestamp);
-                        }
-                    });
-                } else {
-                    // send cached metadata json in response 
-                    _sendMetadata(context, timestamp);
-                }                
-            } else {
+        MetadataUtilities.getMetadata().setHandler(res -> {
+            if(res.failed()) {
                 context.response()
                     .putHeader("Content-Type", "text/plain")
                     .setStatusCode(HttpURLConnection.HTTP_INTERNAL_ERROR)
-                    .setStatusMessage("Could not access metadata.")
+                    .setStatusMessage("Could not access metadata: " + res.cause())
                     .end();
-                System.err.println("Could not get properties for metadata " +
-                    _metadataFileName + ": " + propsResult.cause().getMessage());
-                _server.log(context.request(), context.user(), HttpURLConnection.HTTP_INTERNAL_ERROR, timestamp);
-            }                
-        });
-    }
+            } else {
+                
 
-    /** Send metadata JSON in response.
-     * @param context The vertx routing context.
-     * @param timestamp The timestamp of the request.
-     */
-    private void _sendMetadata(RoutingContext context, long timestamp) {
+                // construct the response json
+                
+                JsonObject principal = context.user().principal();
+                
+                JsonObject returnJson = new JsonObject().mergeIn(principal);                                
+                
+                boolean isAdmin = false;
+                
+                if(returnJson.containsKey("admin")) {
+                    for(Object v: returnJson.getJsonArray("admin")) {
+                        if("*".equals(v) ) {
+                            isAdmin = true;
+                        }
+                    };
+                }
+                
+                // add metadata
         
-        // construct the response json
-        
-        JsonObject principal = context.user().principal();
-        //Set<String> userGroups = AuthUtilities.getGroups(context.user());        
-        
-        JsonObject returnJson = new JsonObject().mergeIn(principal);
-        
-        // add the username
-        /*
-        returnJson.put("username", principal.getString("username"));
-        returnJson.put("fullname", principal.getString("fullname"));
-        
-        // add the groups
-        JsonArray groupsArray = new JsonArray();
-        for(String groupName : userGroups) {
-            groupsArray.add(groupName);
-        }
-        returnJson.put("groups", groupsArray);
-        */
-        
-        boolean isAdmin = false;
-        for(Object g: returnJson.getJsonArray("groups")) {
-            if(g.equals("admin")) {
-                isAdmin = true;
-                break;
-            }
-        }
-
-        // add metadata
-
-        if(isAdmin) {
-            returnJson.put("metadata", _metadataJson.copy());
-        } else { 
-            JsonArray returnMetadataJson = new JsonArray();
-            for(int i = 0; i < _metadataJson.size(); i++) {
-                JsonObject m = _metadataJson.getJsonObject(i);
-                boolean inGroup = false;
-                if(!m.containsKey("groups")) {
-                    inGroup = true;
-                } else {
-                    JsonArray groupsList = m.getJsonArray("groups");
-                    for(int j = 0; j < groupsList.size(); j++) {
-                        for(Object g: returnJson.getJsonArray("groups")) {
-                            if(g.equals(groupsList.getString(j))) {
-                                inGroup = true;
-                                break;
+                JsonArray metadataJson = res.result();
+                
+                if(isAdmin) {
+                    returnJson.put("metadata", metadataJson.copy());
+                } else { 
+                    JsonArray returnMetadataJson = new JsonArray();
+                    for(int i = 0; i < metadataJson.size(); i++) {
+                        JsonObject m = metadataJson.getJsonObject(i);
+                        boolean inGroup = false;
+                        if(!m.containsKey("groups")) {
+                            inGroup = true;
+                        } else {
+                            JsonArray groupsList = m.getJsonArray("groups");
+                            for(int j = 0; j < groupsList.size(); j++) {
+                                for(Object g: returnJson.getJsonArray("groups")) {
+                                    if(g.equals(groupsList.getString(j))) {
+                                        inGroup = true;
+                                        break;
+                                    }
+                                }
                             }
+                        }
+                        
+                        if(inGroup) {
+                            returnMetadataJson.add(m.copy());
+                        }
+                    };
+                                
+                    returnJson.put("metadata", returnMetadataJson);
+                }
+                
+                // remove specific fields before returning to client.
+                for(int i = 0; i < returnJson.getJsonArray("metadata").size(); i++) {
+                    JsonObject obj = returnJson.getJsonArray("metadata").getJsonObject(i);
+                    for(String key: _fieldsToRemove) {
+                        if(obj.containsKey(key)) {
+                            obj.remove(key);
                         }
                     }
                 }
+                       
+                //System.out.println(returnJson.encodePrettily());
                 
-                if(inGroup) {
-                    returnMetadataJson.add(m.copy());
-                }
-            };
-                        
-            returnJson.put("metadata", returnMetadataJson);
-        }
-        
-        // remove specific fields before returning to client.
-        for(int i = 0; i < returnJson.getJsonArray("metadata").size(); i++) {
-            JsonObject obj = returnJson.getJsonArray("metadata").getJsonObject(i);
-            for(String key: _fieldsToRemove) {
-                if(obj.containsKey(key)) {
-                    obj.remove(key);
-                }
+                context.response().putHeader("Content-Type", "application/json")
+                    .end(returnJson.encode());
+                _server.log(context.request(), context.user(), HttpURLConnection.HTTP_OK, timestamp);
             }
-        }
-               
-        //System.out.println(returnJson.encodePrettily());
+        });
         
-        context.response().putHeader("Content-Type", "application/json")
-            .end(returnJson.encode());
-        _server.log(context.request(), context.user(), HttpURLConnection.HTTP_OK, timestamp);
     }
-    
-    /** Vertx file system. */
-    private FileSystem _fileSystem;
-    
-    /** Metadata file name. */
-    private String _metadataFileName;
-    
-    /** Last modified time of metadata file. */
-    private long _metadataLastModifiedTime = -1;
-    
-    /** Metadata JSON cached from reading metadata file. */
-    private JsonArray _metadataJson;
-    
+        
     /** Names of fields to remove from metadata json before returning to the client. */
     private static String[] _fieldsToRemove = {"groups", "private"};
 }
